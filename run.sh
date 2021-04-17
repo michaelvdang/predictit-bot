eab10cc39ff65e222b3adeff7c4121c4ee41b165
dalton-b <daltonbassett@gmail.com>
2021-04-17 17:28:25 -0400

    Hopefully added all the necessary stuff to run a daily action

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Starting run.sh..."
+echo "Creating virtual environment..."
+python3 -m venv ./env
+echo "Activating environment..."
+source ./env/bin/activate
+echo "Installing packages..."
+pip3 install -e .
+echo "Running the project..."
+python3 Driver.py
+echo "Setting GitHub username..."
+git config user.name "dalton-b"
+echo "Staging changes..."
+git add .
+echo "Changes staged!"
