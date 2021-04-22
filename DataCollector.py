049be433153b9c94ef22428005b7b1216cc31d7b
dalton-b <daltonbassett@gmail.com>
2021-04-21 23:09:28 -0400

    DataCollector now writes JSON

--- DataCollector.py
+++ DataCollector.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import datetime
+import json
 
 
 class DataCollector:
@@ -15,7 +16,7 @@ class DataCollector:
         data_filepath = create_data_filepath(current_time)
 
         with open(data_filepath, "w") as text_file:
-            print(self.response, file=text_file)
+            json.dump(self.response, text_file)
 
     @property
     def response(self):
